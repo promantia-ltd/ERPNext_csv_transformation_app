9b4c720560cb59911f56f0ae5be9b37fec81fe99
paul.clinton <paul.clinton@promantia.com>
2020-02-05 16:28:26 +0530

    [6511] Adding compatibility for python 3.5

--- csv_transformation/basic_transform/transform.py
+++ csv_transformation/basic_transform/transform.py
@@ -10,14 +10,17 @@ def transformFile(masterFilePath=None) :
     from frappe.utils.csvutils import read_csv_content
     if isValidPath("/"+masterFilePath):
         jsonData=getJsonMap()
+        print("Initiating transformation...")
         for key in jsonData:
             templateRows=getTemplate(key)
             mainFileData=getMainData("/"+masterFilePath)
+            print("Transformation started for",key,".Please wait...")
             if checkIfJsonArray(jsonData[key]):
                 for key1 in jsonData[key]:
                     mainFileData=getMainData("/"+masterFilePath)
                     mappedData=getMappedData(templateRows,mainFileData,jsonData[key][key1])
                     saveTemplateWithData(key,mappedData)
+                print("Transformation completed for ",key)
                 print("Successfully created the file at ",Path(__file__).parent / ("output/"+str(key)+".csv"))
             else:
                 mappedData=getMappedData(templateRows,mainFileData,jsonData[key])
@@ -40,7 +43,13 @@ def getMappedData(templateContent,mainContent,jsonMap):
     templateColumn=templateContent[15]
     dataColumn=mainContent.pop(0)
     listArray=[]
+    itemGroupList=set()
+    itemCodeList=set()
+    append=True
+    for value in mainContent:
+        itemGroupList.add(value[dataColumn.index('Type')])
     for index,val in enumerate(mainContent):
+        append=True
         listArray=[]
         for i in templateColumn:
             listArray.append(None)
@@ -49,38 +58,43 @@ def getMappedData(templateContent,mainContent,jsonMap):
                 if(not (val[dataColumn.index(jsonData["source"])] and val[dataColumn.index(jsonData["source"])].strip())):
                     listArray[templateColumn.index(jsonData["destination"])]=jsonData["default"]
                 else:
-                    listArray[templateColumn.index(jsonData["destination"])]=val[dataColumn.index(jsonData["source"])]
+                    itemCode=val[dataColumn.index('Product #')]
+                    if not(itemCode in itemGroupList):
+                        listArray[templateColumn.index(jsonData["destination"])]=val[dataColumn.index(jsonData["source"])] 
+                    else:
+                        append=False
             except ValueError:
                 try:
                     listArray[templateColumn.index(jsonData["destination"])]=jsonData["default"]
                 except KeyError:
                     print(val[dataColumn.index(jsonData["source"])])
-        templateContent.append(listArray)
+        if append:
+            templateContent.append(listArray)
     return templateContent
 
 
 def getJsonMap():
     jsonMapPath = Path(__file__).parent / "json_maps/item-data.json"
-    with open(jsonMapPath) as jsonfile:
+    with open(str(jsonMapPath),'r') as jsonfile:
         jsonData = json.load(jsonfile)
     return jsonData
 
 
 def getTemplate(doctypeName):
     templatePath=Path(__file__).parent / ("data/"+str(doctypeName)+".csv")
-    with open(templatePath,'r') as tempcsvfile:
+    with open(str(templatePath),'r') as tempcsvfile:
         templateContent=read_csv_content(tempcsvfile.read())
     return templateContent
 
 
 def getMainData(fileLocation):
-    with open(fileLocation,'r') as csvFile:
+    with open(str(fileLocation),'r') as csvFile:
         fileContent=read_csv_content(csvFile.read())
     return fileContent
 
 
 def saveTemplateWithData(fileName,mappedData):
-    with open(Path(__file__).parent / ("output/"+str(fileName)+".csv"), 'w', newline='') as file:
+    with open(str(Path(__file__).parent / ("output/"+str(fileName)+".csv")), 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerows(mappedData)
 
